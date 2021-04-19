905b4c293685a25ceaab62f5f650f7e19982b735
Atul Gupta <atulgupta.personal@gmail.com>
2021-04-19 09:54:55 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { PortalModule } from '@angular/cdk/portal';
 import { AppComponent } from './app.component';
 import { CustomSelectComponent } from './custom-dropdown/custom-select.component';
 import { CustomSelectOptionComponent } from './custom-dropdown/custom-select-option.component';
@@ -16,7 +18,9 @@ import { DropdownComponent } from './custom-dropdown/dropdown.component';
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    OverlayModule,
+    PortalModule
   ],
   bootstrap: [AppComponent]
 })
